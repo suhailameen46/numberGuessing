f38f7a772742b0f0081f6b3277d0b858105b2421
suhail <suhailameen46@gmail.com>
2015-11-17 07:02:15 +0530

    version 1.0

--- whatNumber/ViewController.swift
+++ whatNumber/ViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var correct = arc4random_uniform(6)
+    
+    @IBOutlet var messgae: UILabel!
+    
+    @IBOutlet var guessField: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print(correct)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func checkButton(sender: UIButton) {
+        
+        let guess = guessField.text
+        let correctGuess = String(correct)
+        guessField.text = ""
+        
+        if guess == correctGuess {
+            
+            messgae.text = "Yaay! correct Answer"
+            messgae.textColor = UIColor.greenColor()
+            
+        
+        }
+        
+        else {
+            messgae.text = "wrong answer! Try again"
+            messgae.textColor = UIColor.redColor()
+            
+        
+        }
+        
+        
+        
+    }
 
 }
 
